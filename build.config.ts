3325d8faf3d1692f1cb56a48e9d0dc78ce7f1db9
Sebastian Langer <sebastian.langer@laioutr.com>
2025-10-30 12:09:53 +0100

    chore: prepare release

--- build.config.ts
+++ build.config.ts
@@ -5,5 +5,8 @@ export default defineBuildConfig({
   externals: [
     // Add other external dependencies if you have them...
     "defu",
+    "@parcel/watcher",
+    "@laioutr-core/frontend-core",
+    "@laioutr-core/kit",
   ],
 });
